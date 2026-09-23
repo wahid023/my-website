c639ad752ad8c94acd94772ce5ad1921e1e66eff
wahid023 <147054135+wahid023@users.noreply.github.com>
2026-09-23 21:10:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,23 @@ navItems.forEach((item) => {
     });
 
 });
+// ================================
+// DARK / LIGHT MODE
+// ================================
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        themeBtn.textContent = "🌙";
+    } else {
+        themeBtn.textContent = "☀️";
+    }
+
+});
 
 
 // ================================
