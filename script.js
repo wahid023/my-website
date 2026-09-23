4f14cad23e57ae834e320864258093226f9399b7
wahid023 <147054135+wahid023@users.noreply.github.com>
2026-09-23 22:01:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -130,3 +130,25 @@ const revealOnScroll = () => {
 window.addEventListener("scroll", revealOnScroll);
 
 revealOnScroll();
+// BACK TO TOP BUTTON
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 400) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
